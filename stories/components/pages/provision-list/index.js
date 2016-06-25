f6ae8fc407e95c6f73f1d325604de8faef6ee348
Goran Mekić <meka@tilda.center>
2016-06-25 03:14:29 +0200

    Add provision list component

---
+++ stories/components/pages/provision-list/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../../layouts/layout';
+
+
+export default function ProvisionList() {
+  return (
+    <Layout title="Provisions" provision="active">
+      <div className="provision__ok">1341324fb134f134</div>
+      <div className="provision__failed">56a3b4fb134f87ec</div>
+    </Layout>
+  );
+}
